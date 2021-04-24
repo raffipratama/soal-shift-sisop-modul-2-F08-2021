e395d6b960118839a2f998502970b6a8088d9281
enemiy <nmihsan25@gmail.com>
2021-04-24 23:50:19 +0700

    Revisi soal1

--- soal1/soal1.c
+++ soal1/soal1.c
@@ -43,8 +43,13 @@ int main() {
   close(STDERR_FILENO);
 
   while (1) {
+	time_t currTm;
+	currTm = time(NULL);
+	struct tm currTime = *localtime(&currTm);
+
+	if( currTime.tm_sec==00 && currTime.tm_min==22 && currTime.tm_hour==16 && currTime.tm_mday==9 && currTime.tm_mon == 3){
   
-        pid_t child_id;
+    	pid_t child_id;
 		int status;
 
         child_id = fork();
@@ -64,17 +69,17 @@ int main() {
             }
 
             if (child_id2 ==0 ){
-            char *argv[] = {"mkdir", "-p", "Musyik","Fylm","Pyoto" ,NULL};
-            execv("/usr/bin/mkdir", argv);}
+            char *argv1[] = {"mkdir", "-p", "Musyik","Fylm","Pyoto" ,NULL};
+            execv("/usr/bin/mkdir", argv1);}
 
             else{
-               while ((wait(&status_2)) > 0);
-               pid_t child_id3;
-               int status3;
+               	while ((wait(&status_2)) > 0);
+               	pid_t child_id3;
+               	int status3;
 
-               child_id3 = fork();
+               	child_id3 = fork();
 
-               if (child_id3 < 0) {
+                if (child_id3 < 0) {
                 exit(EXIT_FAILURE);
                 }
 
@@ -109,13 +114,13 @@ int main() {
                             }
                        	 
                         	if(child_id6==0){
-                               	char *argv[] = {"wget","-q","--no-check-certificate","https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download","-O","Foto_for_Stevany.zip", NULL};
-                            	execv("/usr/bin/wget",argv);
+                               	char *argv2[] = {"wget","-q","--no-check-certificate","https://drive.google.com/uc?id=1FsrAzb9B5ixooGUs0dGiBr-rC7TS9wTD&export=download","-O","Foto_for_Stevany.zip", NULL};
+                            	execv("/usr/bin/wget",argv2);
                         	}
                         	else{
                             	while((wait(&status6)) > 0);
-                            	char *argv[] = {"unzip","Foto_for_Stevany.zip","-d","/home/ihsannm/soal-shift-sisop-modul-2-F08-2021/soal1", NULL};
-                            	execv("/usr/bin/unzip", argv);
+                            	char *argv3[] = {"unzip","-j","Foto_for_Stevany.zip","-d","/home/ihsannm/soal-shift-sisop-modul-2-F08-2021/soal1/Pyoto", NULL};
+                            	execv("/usr/bin/unzip", argv3);
                         	}
                     	}
                     	else{
@@ -130,16 +135,17 @@ int main() {
                             }
                        	 
                         	if(child_id7 == 0){
-                            	char *argv[] = {"wget","-q","--no-check-certificate","https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download","-O","Musik_for_Stevany.zip", NULL};
-                            	execv("/usr/bin/wget", argv);
+                            	char *argv4[] = {"wget","-q","--no-check-certificate","https://drive.google.com/uc?id=1ZG8nRBRPquhYXq_sISdsVcXx5VdEgi-J&export=download","-O","Musik_for_Stevany.zip", NULL};
+                            	execv("/usr/bin/wget", argv4);
                         	}
                         	else{
                             	while((wait(&status7)) > 0);
-                            	char *argv[] = {"unzip","Musik_for_Stevany.zip","-d","/home/ihsannm/soal-shift-sisop-modul-2-F08-2021/soal1", NULL};
-                            	execv("/usr/bin/unzip", argv);
+                            	char *argv5[] = {"unzip","-j","Musik_for_Stevany.zip","-d","/home/ihsannm/soal-shift-sisop-modul-2-F08-2021/soal1/Musyik", NULL};
+                            	execv("/usr/bin/unzip", argv5);
                         	}
                     	}
-                	}
+					}
+					
                 	else{
                     	while((wait(&status4)) > 0);
                     	pid_t child_id8;
@@ -152,19 +158,30 @@ int main() {
                         }
 
                     	if(child_id8 == 0){
-                        	char *argv[] = {"wget","-q","--no-check-certificate","https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download","-O","Film_for_Stevany.zip", NULL};
-                           	execv("/usr/bin/wget", argv);
+                        	char *argv6[] = {"wget","-q","--no-check-certificate","https://drive.google.com/uc?id=1ktjGgDkL0nNpY-vT7rT7O6ZI47Ke9xcp&export=download","-O","Film_for_Stevany.zip", NULL};
+                           	execv("/usr/bin/wget", argv6);
                     	}
                     	else{
                         	while((wait(&status8)) > 0);
-                        	char *argv[] = {"unzip","Film_for_Stevany.zip","-d","/home/ihsannm/soal-shift-sisop-modul-2-F08-2021/soal1", NULL};
-                        	execv("/usr/bin/unzip", argv);
+                        	char *argv7[] = {"unzip","-j","Film_for_Stevany.zip","-d","/home/ihsannm/soal-shift-sisop-modul-2-F08-2021/soal1/Fylm", NULL};
+                        	execv("/usr/bin/unzip", argv7);
                     	}
                 	}
                	}
                	else{
                    	while((wait(&status3)) > 0);
-                   	pid_t child_id9;
+                }
+			}
+    	} 
+		else{
+			while((wait(&status)) > 0);
+		}
+	}
+	 
+	  
+	 
+	  else if( currTime.tm_sec==00 && currTime.tm_min==22 && currTime.tm_hour==22 && currTime.tm_mday==9 && currTime.tm_mon == 3){
+		      		pid_t child_id9;
                    	int status9;
                   	
                     child_id9 = fork();
@@ -174,32 +191,18 @@ int main() {
                     }
 
                    	if(child_id9==0){
-                       	pid_t child_id10;
-                       	int status10;
-
-                        child_id10 = fork();
-                      	 
-                       	if(child_id10<0) {
-                        exit(EXIT_FAILURE);
-                         }
-
-                    	if(child_id10==0){
-							
-                    	}
-                    	else{
-                        	while((wait(&status10)) > 0);
-                    	}
+                      char *argv8[]={"zip","-rm","Lopyu_Stevany.zip","Fylm","Musyik","Pyoto", NULL};
+					  execv("/usr/bin/zip",argv8);
                 	}
                 	else{
                     	while((wait(&status9)) > 0);
                 	}
             	}
-        	}
-         
-    	}else{
-			while((wait(&status)) > 0);
-		}
+
+	  }
+	  sleep(1);
+
 	}
-}
+
 
  
